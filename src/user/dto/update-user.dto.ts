7235e7fee9e0b6e6c07ebf93ec41639fbb4ee381
Moaane <gpro0150@gmail.com>
2023-08-26 22:22:28 +0700

    user crud

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,36 @@
+import { OmitType } from '@nestjs/mapped-types';
+import { UserEntity } from "../entities/user.entity";
+import { IsNotEmpty, IsEmail, IsDate, IsOptional } from "@nestjs/class-validator";
+
+export class UpdateUserDto extends OmitType(UserEntity, [
+    'createdAt', 'updatedAt'
+]) {
+    @IsNotEmpty()
+    name: string;
+
+    @IsNotEmpty()
+    nik: string
+
+    @IsNotEmpty()
+    placeOfBirth: string
+
+    @IsOptional()
+    @IsDate()
+    dateOfBirth: Date
+
+    @IsNotEmpty()
+    biologicalMotherName: string
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string
+
+    @IsNotEmpty()
+    referralCode: string
+
+    @IsNotEmpty()
+    username: string
+
+    @IsNotEmpty()
+    password: string
+}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { User as UserModel } from "@prisma/client";
+
+export class UserEntity implements UserModel {
+    id: string;
+    name: string;
+    nik: string
+    placeOfBirth: string
+    dateOfBirth: Date
+    biologicalMotherName: string
+    email: string
+    referralCode: string
+    username: string
+    password: string
+    createdAt: Date;
+    updatedAt: Date;
+}
