89f759aad547e9fdc458fc08371f1b5561ad9b1a
FathirTHX <fathirisgaming12@gmail.com>
2023-08-28 14:05:41 +0700

    auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body} from '@nestjs/common';
+import { Controller, Post, Body, UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login-auth.dto';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('login')
 export class AuthController {
-    constructor(private authService: AuthService) { }
-
+  constructor(private authService: AuthService) { }
+  
+    @UseGuards(LocalAuthGuard )
     @Post('')
     async create(@Body() loginDto: LoginDto) {
       return await this.authService.login(loginDto);
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors
-  await app.listen(process.env.PORT,'192.168.1.6',()=>{
+  await app.listen(process.env.PORT,'192.168.18.210',()=>{
     console.log('BERHASIL TOD')
   });
 }
