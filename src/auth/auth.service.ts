7bee37c60147ee6ad10f53be38e83bc85b59f38a
Moaane <gpro0150@gmail.com>
2023-09-08 13:56:52 +0700

    .

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,57 +3,97 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/login-auth.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-
-    
     constructor(
         private prisma: PrismaService,
         private jwt: JwtService
     ) { }
 
-        
-
-    async validateUser(loginDto: LoginDto): Promise<{ token: string }> {
-        const isUserValid = await this.prisma.users.findUnique({
-            where: { username: loginDto.username }
+    async register(
+        full_name: string,
+        identity_number: string,
+        birth_date: Date,
+        birth_place: string,
+        mother_maiden_name: string,
+        email: string,
+        username: string,
+        password: string
+    ) {
+        const customer = await this.prisma.customers.create({
+            data: {
+                full_name,
+                identity_number,
+                birth_date,
+                birth_place,
+                mother_maiden_name,
+                email
+            }
         })
 
-        if (!isUserValid) {
-            throw new NotFoundException(`No user found for username: ${loginDto.username}`);
+        const isPasswordValid = await this.prisma.users.findFirst({
+            where: { password: password }
+        })
+
+        if (isPasswordValid.password.length < 6) {
+            return new UnauthorizedException('Password less than 6 characters')
         }
 
-        const isPasswordValid = await bcrypt.compare(
-            loginDto.password, isUserValid.password
-        )
+        const hashPassword = await bcrypt.hash(isPasswordValid.password, 10)
 
-        if (loginDto.password.length < 6) {
-            throw new UnauthorizedException('Password less than 6 characters');
-        }
+        const user = await this.prisma.users.create({
+            data: {
+                username: username,
+                password: hashPassword,
+                ...UserEntity,
+                customer_id: customer.id
+            }
+        })
 
-        if (!isPasswordValid) {
-            throw new UnauthorizedException('Wrong Password');
-        }
-
-        const payload = { sub: isUserValid.id,username: isUserValid.username, name: isUserValid.name, email: isUserValid.email };
-
-        return {token : this.jwt.sign (payload) }
-
+        return { customer, user }
     }
 
-    async login(loginDto: LoginDto) {
-        const isUserValid = await this.prisma.user.findUnique({
-            where: { username: loginDto.username }
-        })
+    // async validateUser(loginDto: LoginDto): Promise<{ token: string }> {
+    //     const isUserValid = await this.prisma.users.findUnique({
+    //         where: { username: loginDto.username }
+    //     })
 
-        if (!isUserValid) {
-            throw new NotFoundException(`No user found for username: ${loginDto.username}`);
-        }
+    //     if (!isUserValid) {
+    //         throw new NotFoundException(`No user found for username: ${loginDto.username}`);
+    //     }
 
-        const payload = { sub: isUserValid.id,username: isUserValid.username, name: isUserValid.name, email: isUserValid.email };
+    //     const isPasswordValid = await bcrypt.compare(
+    //         loginDto.password, isUserValid.password
+    //     )
 
-        return {token : this.jwt.sign (payload) }
-    } 
-    
+    //     if (loginDto.password.length < 6) {
+    //         throw new UnauthorizedException('Password less than 6 characters');
+    //     }
+
+    //     if (!isPasswordValid) {
+    //         throw new UnauthorizedException('Wrong Password');
+    //     }
+
+    //     const payload = { sub: isUserValid.id, username: isUserValid.username, name: isUserValid.name, email: isUserValid.email };
+
+    //     return { token: this.jwt.sign(payload) }
+
+    // }
+
+    // async login(loginDto: LoginDto) {
+    //     const isUserValid = await this.prisma.user.findUnique({
+    //         where: { username: loginDto.username }
+    //     })
+
+    //     if (!isUserValid) {
+    //         throw new NotFoundException(`No user found for username: ${loginDto.username}`);
+    //     }
+
+    //     const payload = { sub: isUserValid.id, username: isUserValid.username, name: isUserValid.name, email: isUserValid.email };
+
+    //     return { token: this.jwt.sign(payload) }
+    // }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,23 +3,14 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Customers, Users } from '@prisma/client';
+import { CustomerEntity } from 'src/customer/entities/customer.entity';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma:PrismaService){}
 
-  async create(
-    full_name: string,
-    identity_number: string,
-    birth_date: Date,
-    birth_place: string,
-    mother_maiden_name: string,
-    username: string,
-    password: string
-  ){
-    return await this.prisma.customers.create() ;
-  }
-
+  
   findAll() {
     return `This action returns all users`;
   }
